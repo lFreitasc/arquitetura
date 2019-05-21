928b73adef566f4af26a1ffdbf0943ffe3752e52
Lucas Freitas <lucas.freitascosta96@gmail.com>
2019-05-21 14:17:30 -0300

    asd

---
+++ src/main/java/br/utfpr/architecture/controllers/professorController.java
@@ -0,0 +1,14 @@
+package br.utfpr.architecture.controllers;
+
+import br.utfpr.architecture.Entity.AulasDoPlanoSubstituicao;
+
+/**
+ * professorController
+ */
+public class professorController {
+    
+    public void solicitarSubstituicao(){
+        AulasDoPlanoSubstituicao aulasDoPlanoSubstituicao =  new AulasDoPlanoSubstituicao();
+        
+    }
+}
